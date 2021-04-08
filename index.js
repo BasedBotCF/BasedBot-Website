e917f452a81752de29235fbc44b83acbf7c54dc5
Kaz <58613384+KazUpTV@users.noreply.github.com>
2021-04-07 20:42:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,14 @@ var path = require('path');
 const port = 5000
 
 app.use('/public', express.static(__dirname + '/public'));
-app.use('vaildation key', express.static(__dirname + 'vaildation key'));
+app.use('/css', express.static(__dirname + '/css'));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 app.get('', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.get('/basedbotinfo', function(req, res) {
-  res.sendFile(path.join(__dirname + '/basedbotinfo.html'));
-});
 app.get('/checkhealth', function(req, res) {
   res.sendFile(path.join(__dirname + '/checkhealth.html'));
-});
-app.get('/github', function(req, res) {
-  res.sendFile(path.join(__dirname + '/github.html'));
 });
 app.get('/commands', function(req, res) {
   res.sendFile(path.join(__dirname + '/commands.html'));
@@ -27,7 +21,6 @@ app.get('/commands', function(req, res) {
 app.get('/favicon.ico', function(req, res) {
   res.sendFile(path.join(__dirname + '/favicon.ico'));
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
